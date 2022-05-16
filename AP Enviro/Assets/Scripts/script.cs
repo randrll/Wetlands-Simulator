32ee78df401927b872da38aa260a6719babd2344
randrll <randell313@gmail.com>
2022-05-15 20:41:32 -0700

    attempted fix

--- AP Enviro/Assets/Scripts/script.cs
+++ AP Enviro/Assets/Scripts/script.cs
@@ -7,8 +7,12 @@ public class script : MonoBehaviour
 {
     public void startSimulation() {
 <<<<<<< HEAD:AP Enviro/Assets/Scripts/script.cs
+<<<<<<< HEAD:AP Enviro/Assets/Scripts/script.cs
         Debug.Log("Clicked!");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // testing
+=======
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+>>>>>>> parent of 05c6957 (testing):AP Enviro/Assets/script.cs
 =======
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 >>>>>>> parent of 05c6957 (testing):AP Enviro/Assets/script.cs
