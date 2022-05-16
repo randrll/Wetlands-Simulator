9c8c7895db78e92ac8162593a0c89f3aef80107d
randrll <randell313@gmail.com>
2022-05-15 20:39:58 -0700

    Update script.cs

--- AP Enviro/Assets/Scripts/script.cs
+++ AP Enviro/Assets/Scripts/script.cs
@@ -6,7 +6,11 @@ using UnityEngine.SceneManagement;
 public class script : MonoBehaviour
 {
     public void startSimulation() {
+<<<<<<< HEAD:AP Enviro/Assets/Scripts/script.cs
         Debug.Log("Clicked!");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // testing
+=======
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+>>>>>>> parent of 05c6957 (testing):AP Enviro/Assets/script.cs
     }
 }
