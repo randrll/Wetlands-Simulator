c18c19e8714891cac1ef47bda5f644bb2696da50
randrll <randell313@gmail.com>
2022-05-20 22:40:59 -0700

    sliders script + graphs

--- AP Enviro/Assets/Scripts/environment.cs
+++ AP Enviro/Assets/Scripts/environment.cs
@@ -5,10 +5,39 @@ using UnityEngine.UI;
 
 public class environment : MonoBehaviour 
 {
-    private int pH = 7, totalCarbon = 0, waterTemperature = 40, turbidity = 0;
+    private int pH, totalCarbon, waterTemp, turbidity, biodiversity, dissolvedOxygen, biochemicalOxygenDemand; 
 
-    [SerializeField] private Slider carbonSlider, sedimentRSlider, nutrientRSlider;
+    [SerializeField] private Slider carbonSlider, sedimentRSlider, nutrientRSlider, municipalWasteSlider, sewageSlider, heavyMetalsSlider;
 
+    public environment() {
+        pH = 7; totalCarbon = 0; waterTemp = 0; turbidity = 0; biodiversity = 0; dissolvedOxygen = 10; biochemicalOxygenDemand = 5; // temporary values 
+    }
 
+    public int getpH() {
+        return pH;
+    }
 
+    public int getCarbon() {
+        return totalCarbon;
+    }
+
+    public int getWaterTemp() {
+        return waterTemp;
+    }
+
+    public int getTurbidity() {
+        return turbidity;
+    }
+
+    public int getBiodiversity() {
+        return biodiversity;
+    }
+
+    public int getDissolvedOxygen() {
+        return dissolvedOxygen;
+    }
+
+    public int getOxyDemand() {
+        return biochemicalOxygenDemand;
+    }
 }
