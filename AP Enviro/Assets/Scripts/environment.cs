c5923506fc11abf95ce7b81e43a3026ed9c55239
randrll <randell313@gmail.com>
2022-05-20 21:52:52 -0700

    yes

--- AP Enviro/Assets/Scripts/environment.cs
+++ AP Enviro/Assets/Scripts/environment.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class environment
+public class environment : MonoBehaviour 
 {
-    private int pH = 7;
-    private int totalCarbon = 0;
-    private int waterTemperature = 40;
-    private int turbidity = 0;
-    
+    private int pH = 7, totalCarbon = 0, waterTemperature = 40, turbidity = 0;
+
+    [SerializeField] private Slider carbonSlider, sedimentRSlider, nutrientRSlider;
+
+
+
 }
