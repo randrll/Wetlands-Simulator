3c916dcb9a0f74c646958ac8dce36208c0f774e3
Doku98 <ckumagai98@gmail.com>
2022-05-21 17:26:25 -0700

    Worked on Animal sprites and animal class

--- AP Enviro/Assets/Scripts/AnimalClass.cs
+++ AP Enviro/Assets/Scripts/AnimalClass.cs
@@ -2,7 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimalClass : MonoBehaviour {
+public class AnimalClass : MonoBehaviour
+{
+
+
+    /*
+    0 = dead
+    1 = sick
+    2 = okay
+    3 = very healthy
+
+    Walkable (not done): checks if tile is walkable
+    reproduce(done): returns if two animals reproduce 
+    test(done): just for testing random things
+    move(not done): not done bc calls walkable with is not done
+        1. create rng 1-4,
+        2. check if random direction is walkable. if true, move there. If false, go back to step 1
+    
+    move and reproduce should be called in update()
+
+    */
     private int hp = -1;
     private int x = -1;
     private int y = -1;
@@ -20,38 +39,125 @@ public class AnimalClass : MonoBehaviour {
         return true;
     }
 
-    public void NewRandomNumber()
+    public static int  NewRandomNumber(int min, int max)
     {
         int randomNumber;
         int lastNumber;
-        randomNumber = Random.Range(0, 10);
-        Debug.Log(randomNumber);
+        randomNumber = Random.Range(min, max);
+        return randomNumber;
     }
 
- /*   public void move()
+
+    public static bool reproduce(int hp, int x1, int y1, int x2, int y2, int distance)
     {
-        int randomNumber = randomNumber.Range(1, 4);
-        Debug.Log(randomNumber);
-        Debug.Log(randomNumber);
-        Debug.Log(randomNumber);
-        Debug.Log(randomNumber);
+       if (Mathf.Abs(x1 - x2) <= distance && Mathf.Abs(y1 - y2) <= distance)
+       {
+
+            if(hp == 3)
+            {
+                if(NewRandomNumber(1,10) <= 8)
+                {
+                    return true;
+                }
+            }
+            if(hp == 2)
+            {
+                if(NewRandomNumber(1,10) <= 5)
+                {
+                    return true;
+                }
+            }
+            if(hp == 1)
+            {
+                if(NewRandomNumber(1,10) == 1)
+                {
+                    return true;
+                }
+            }
+            
+       }
+       return false;
     }
- */
+
+
+    //method for testing
+    public static void test()
+    {
+        Debug.Log("healty");
+        for (int i = 0; i < 10; i++)
+        {
+            if (reproduce(3, 1, 1, 1, 1, 10))
+            {
+                Debug.Log("reproduce");
+            }
+            else
+            {
+                Debug.Log("reproduce failed");
+            }
+        }
+
+        Debug.Log("okay");
+        for (int i = 0; i < 10; i++)
+        {
+            if (reproduce(2, 1, 1, 1, 1, 10))
+            {
+                Debug.Log("reproduce");
+            }
+            else
+            {
+                Debug.Log("reproduce failed");
+            }
+        }
+    }
+    //uses rng numbers to move
+    //1 is right
+    //2 is left
+    //3 is up
+    //4 is down
+
+    //check if tile in walkable 
+    //if so move there, if not generate a new num/direction and check if the tile is walkable 
+    
+    
+    public void move(int increaseX, int increaseY)
+    {
+        bool moved = false;
+
+        while (!moved)
+        {
+            int direction = NewRandomNumber(1, 4);
+            if (direction == 1)
+            {
+                //call walkable to see if right is open if so move there
+                moved = true; //only if we moved
+            }
+            else if (direction == 2)
+            {
+                //call walkable to see if left is open if so move there
+                moved = true; //only if we moved
+            }
+            else if (direction == 3)
+            {
+                //call walkable to see if up is open if so move there
+                moved = true; //only if we moved 
+            }
+            else
+            {
+                //call walkable to see if down is open if so move there
+                moved = true; //only if we moved
+            }
+        }
+    }
+
 
     // Start is called before the first frame update
     void Start()
-    {
-        //move();
-        Debug.Log("random numbers are below");
-        NewRandomNumber();
-        NewRandomNumber();
-        NewRandomNumber();
-        NewRandomNumber();
-    }
+    { 
 
+    }
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
