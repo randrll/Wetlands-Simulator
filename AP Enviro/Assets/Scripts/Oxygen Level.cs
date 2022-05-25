e9efd38e8122789610f5e308637a666d4e25704f
randrll <randell313@gmail.com>
2022-05-25 00:28:25 -0700

    Final (maybe)

--- AP Enviro/Assets/Scripts/Oxygen Level.cs
+++ AP Enviro/Assets/Scripts/Oxygen Level.cs
@@ -1,45 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
 
-public class Window_Graph : MonoBehaviour
-{
-    [SerializeField] private Sprite circleSprite;
-    private RectTransform graphContainer;
+// public class Window_Graph : MonoBehaviour
+// {
+//     [SerializeField] private Sprite circleSprite;
+//     private RectTransform graphContainer;
 
-    private void Awake()
-    {
-                Debug.Log("MADE A CIRCLE");
+//     private void Awake()
+//     {
+//                 Debug.Log("MADE A CIRCLE");
        
-        graphContainer = transform.Find("GraphContainer").GetComponent<RectTransform>();
+//         graphContainer = transform.Find("GraphContainer").GetComponent<RectTransform>();
 
       
-      //  add(environment.getPH*2,4);
-        add(50,4);
-       // add(50,4);
-        //add(50,4);
+//       //  add(environment.getPH*2,4);
+//         add(50,4);
+//        // add(50,4);
+//         //add(50,4);
 
-    }
+//     }
 
-    private void CreateCircle(Vector2 anchoredPosition)
-    {
-        GameObject gameObject = new GameObject("circle", typeof(Image));
-        gameObject.transform.SetParent(graphContainer, false);
-        gameObject.GetComponent<Image>().sprite = circleSprite;
-        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
-        rectTransform.anchoredPosition = anchoredPosition;
-        rectTransform.sizeDelta = new Vector2(3, 3);
-        rectTransform.anchorMin = new Vector2(0, 0);
-        rectTransform.anchorMax = new Vector2(0, 0);
-        Debug.Log("MADE A CIRCLE");
-    }
+//     private void CreateCircle(Vector2 anchoredPosition)
+//     {
+//         GameObject gameObject = new GameObject("circle", typeof(Image));
+//         gameObject.transform.SetParent(graphContainer, false);
+//         gameObject.GetComponent<Image>().sprite = circleSprite;
+//         RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+//         rectTransform.anchoredPosition = anchoredPosition;
+//         rectTransform.sizeDelta = new Vector2(3, 3);
+//         rectTransform.anchorMin = new Vector2(0, 0);
+//         rectTransform.anchorMax = new Vector2(0, 0);
+//         Debug.Log("MADE A CIRCLE");
+//     }
     
 
-    private void add(int y, int x){
+//     private void add(int y, int x){
  
 
-        CreateCircle(new Vector2 (x, y));
+//         CreateCircle(new Vector2 (x, y));
         
-    }
-}
+//     }
+// }
